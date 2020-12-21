91e6d43637f6aedeb1406bfdd16f91f517cef6cf
Angelo Rojas <angelog211198@gmail.com>
2020-12-21 02:52:42 -0400

    correcciones

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ let app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/',(req,res) => {
   return res.status(200).json({message:"hola!"})
